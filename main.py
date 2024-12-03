0f893e08b0c34e949b0bb2b3dd17efe8015161ce
Sainath-Chilakalapudi <93853110+Sainath-Chilakalapudi@users.noreply.github.com>
2024-12-03 15:30:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from server import run_server
 from bot.bot_instance import get_bot_instance
 from db.connection import Base, engine
 from helpers.scheduler import start_scheduler
@@ -6,6 +7,7 @@ import asyncio
 from pyrogram import idle
 
 async def main():
+    run_server()  # Start the dummy server
     parser = argparse.ArgumentParser(description="Start the bot.")
     parser.add_argument("--debug", action="store_true", help="Enable debug mode")
     args = parser.parse_args()
